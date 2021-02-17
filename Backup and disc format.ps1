6dd30fc30d318d3f4740e49adf6697a7a60f0727
Cody Wishart <cody.wishart@gmail.com>
2021-02-16 23:15:37 -0600

    Create Backup and disc format.ps1

---
+++ Backup and disc format.ps1
@@ -0,0 +1,14 @@
+# Needs to be combined with main ps1
+
+# Format new disc
+Initialize-Disk -Number 1
+New-Partition -DiskNumber 1 -Size 45gb -DriveLetter E
+Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel Backup -Confirm:$false
+
+# Creating backup scripts
+echo 'robocopy "C:\Users" "E:\Users" /MIR /xj' > "C:\DailyBackup.ps1"
+echo 'robocopy "C:\Windows" E:\OS" /MIR' > "C:\WeeklyBackup.ps1"
+
+# Creating Tasks
+SCHTASKS /Create /SC Daily /TN "Daily Backup" /TR "C:\DailyBackup.ps1" /ST 23:59
+SCHTASKS /Create /SC Weekly /TN "Weekly OS Backup" /TR "C:\WeeklyBackup.ps1" /ST 23:59 /D FRI
